aca29ab5e0288edcea5999cb96b9fdfba2a14f3f
Repy <gamecube02@gmail.com>
2020-06-28 22:18:13 +0900

    awtのuiスケールを1に固定

--- src/main/java/info/repy/foldershow/Main.java
+++ src/main/java/info/repy/foldershow/Main.java
@@ -8,6 +8,7 @@ import java.util.stream.Collectors;
 public class Main {
 
     public static void main(String[] args) {
+        System.setProperty("sun.java2d.uiScale", "1");
         int sec = Integer.parseInt(args[0]);
         String dir = args[1];
         start(sec,dir);
