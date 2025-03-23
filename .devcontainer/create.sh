54b2ec7283d7aa2aaa5e46181c5602f4a099fd98
Repy <gamecube02+git@gmail.com>
2025-03-23 14:24:25 +0000

    dev

--- .devcontainer/create.sh
+++ .devcontainer/create.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 ubuntu_release=`lsb_release -rs`
-wget https://packages.microsoft.com/config/ubuntu/${ubuntu_release}/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
-sudo dpkg -i packages-microsoft-prod.deb
+wget https://packages.microsoft.com/config/ubuntu/${ubuntu_release}/packages-microsoft-prod.deb -O /tmp/packages-microsoft-prod.deb
+sudo dpkg -i /tmp/packages-microsoft-prod.deb
 sudo apt -y install apt-transport-https
 sudo apt -y update
 sudo apt -y install msopenjdk-21
